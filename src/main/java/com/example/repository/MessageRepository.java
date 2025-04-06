71036a787b6dc32424b33d8b08a5393cde8a4106
Emerald Fleur Pham <emerald.pham@hey.com>
2025-04-06 19:09:56 +0000

    Started repository interfaces by extending JpaRepository.

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,11 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.entity.Message;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer>{
+
 }
